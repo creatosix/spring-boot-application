873671b07d9435f560aa4893eb9cd2c9c1f52bd2
devdada <hdada3@uic.edu>
2020-11-04 18:15:11 -0600

    Update

---
+++ conferenceConsumer/conferenceConsumer/src/main/java/com/example/conferenceConsumer/ConferenceConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.conferenceConsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferenceConsumerApplication.class, args);
+	}
+
+}
---
+++ conferenceConsumer/conferenceConsumer/src/test/java/com/example/conferenceConsumer/ConferenceConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.conferenceConsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
